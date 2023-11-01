626ea358c8b832d82a7bf5f7a5434c2145bc8ef5
Arian <arian.foroutan.dev@gmail.com>
2023-11-01 16:12:43 +0330

    add user_id relation

--- lib/data/model/comment.dart
+++ lib/data/model/comment.dart
@@ -3,12 +3,16 @@ class Comment {
   String text;
   String productId;
   String userId;
+  String userthumbnailUrl;
+  String name;
 
   Comment(
     this.id,
     this.text,
     this.productId,
     this.userId,
+    this.userthumbnailUrl,
+    this.name,
   );
 
   factory Comment.fromMapjson(Map<String, dynamic> jsonObject) {
@@ -17,6 +21,8 @@ class Comment {
       jsonObject['text'],
       jsonObject['product_id'],
       jsonObject['user_id'],
+      'http://startflutter.ir/api/files/${jsonObject['expand']['user_id']['collectionName']}/${jsonObject['expand']['user_id']['id']}/${jsonObject['expand']['user_id']['avatar']}',
+      jsonObject['expand']['user_id']['name'],
     );
   }
 }
