41bab847e65a63c863219d2f6a46624b57b8e1ec
Arian <arian.foroutan.dev@gmail.com>
2023-11-02 22:10:33 +0330

    add commentpostloading

--- lib/bloc/comment/bloc/comment_state.dart
+++ lib/bloc/comment/bloc/comment_state.dart
@@ -6,6 +6,18 @@ abstract class CommentState {}
 class CommentLoading extends CommentState {}
 
 class CommentResponse extends CommentState {
-  Either<String, List<Comment>> response;
+  final Either<String, List<Comment>> response;
   CommentResponse(this.response);
+}
+
+class CommentpostLoading extends CommentState {
+  final bool isLoading;
+
+  CommentpostLoading(this.isLoading);
+}
+
+class CommentpostRresponse extends CommentState {
+  final Either<String, String> response;
+
+  CommentpostRresponse(this.response);
 }
