c519f27c0cc88d2d5adfc3c33377ab3f73485d8d
Arian <arian.foroutan.dev@gmail.com>
2023-11-02 22:12:23 +0330

    add postcomment datasource

--- lib/data/datasource/comment_datasource.dart
+++ lib/data/datasource/comment_datasource.dart
@@ -1,12 +1,13 @@
+import 'dart:ffi';
+
 import 'package:dio/dio.dart';
 import 'package:shop_app/data/model/comment.dart';
 import 'package:shop_app/di/di.dart';
 import 'package:shop_app/util/api_exception.dart';
 
 abstract class ICommentDataSource {
-  Future<List<Comment>> getComments(
-    String productId,
-  );
+  Future<List<Comment>> getComments(String productId);
+  Future<void> postComment(String productId, String comment);
 }
 
 class CommentRemoteDatasource extends ICommentDataSource {
@@ -25,6 +26,21 @@ class CommentRemoteDatasource extends ICommentDataSource {
           .map<Comment>((jsonObject) => Comment.fromMapjson(jsonObject))
           .toList();
     } on DioException catch (e) {
+      throw ApiException(e.response?.statusCode, e.response?.data['message']);
+    } catch (e) {
+      throw ApiException(0, 'unknown error');
+    }
+  }
+
+  @override
+  Future<void> postComment(String productId, String comment) async {
+    try {
+      final response = await _dio.post('collections/comment/records', data: {
+        'text': comment,
+        'user_id': 'lkg8xc50i07oedn',
+        'product_id': productId
+      });
+    } on DioError catch (e) {
       throw ApiException(e.response?.statusCode, e.response?.data['message']);
     } catch (e) {
       throw ApiException(0, 'unknown error');
